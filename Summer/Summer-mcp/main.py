72f4256ff3a4edc4c98422585d7447602f1f992d
Anshuman1982 <superbumi82@gmail.com>
2025-07-01 14:43:44 +0530

    Add files via upload

---
+++ Summer/Summer-mcp/main.py
@@ -0,0 +1,5 @@
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv("insurance.csv")
+print(df.head(2))
---
+++ blt/ci-cd-connect/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from ci-cd-connect!")
+
+
+if __name__ == "__main__":
+    main()
